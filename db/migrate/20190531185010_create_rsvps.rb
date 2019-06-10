2975b515c631004ca642da3ff58ba0dd001122fd
learnsometing <brianmonaccio@gmail.com>
2019-06-10 12:53:35 -0400

    Reconfigure create_rsvps migration for postgres

--- db/migrate/20190531185010_create_rsvps.rb
+++ db/migrate/20190531185010_create_rsvps.rb
@@ -3,8 +3,20 @@ class CreateRsvps < ActiveRecord::Migration[5.2]
     create_table :rsvps do |t|
 
       t.timestamps
-      t.references :attended_event, references: :events, foreign_key: true, index: true
-      t.references :attendee, references: :users, foreign_key: true, index: true
+      t.integer :attended_event_id
+      t.integer :attendee_id
     end
+
+    add_foreign_key :rsvps, 
+                    :events, 
+                    column: :attended_event_id
+
+    add_index :rsvps, :attended_event_id
+
+    add_foreign_key :rsvps, 
+                    :events, 
+                    column: :attendee_id
+
+    add_index :rsvps, :attendee_id
   end
 end
